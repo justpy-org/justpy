6e09b3e11e2185729a5a872a62ddbd30bdf944cc
Eli Mintz <eli@dialean.com>
2019-10-26 10:17:52 -0400

    October 25

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
+# python3 setup.py sdist bdist_wheel
+# python3 -m twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+
+# python3 -m pip install -i https://test.pypi.org/simple/ justpy-test==5.0.0
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -6,7 +10,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="justpy-test",
     python_requires=">=3.6",
-    version="0.0.1",
+    version="5.0.0",
     license="Apache",
     author="Eliezer Mintz",
     author_email="eli.mintz@gmail.com",
@@ -15,6 +19,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/pypa/sampleproject",
     packages=setuptools.find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'starlette',
+        'addict',
+    ],
+    # need to add package_data
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
