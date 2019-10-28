e39fa4c717c9a35f4e780e3a9f6677527843df7c
Eli Mintz <eli@dialean.com>
2019-10-28 00:04:49 -0400

    October 27

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="justpy-test",
     python_requires=">=3.6",
-    version="5.0.1",
+    version="5.0.2",
     license="Apache",
     author="Eliezer Mintz",
     author_email="eli.mintz@gmail.com",
