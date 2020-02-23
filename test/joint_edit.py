0198f907bdcf93956532383aaeac46431ba51021
Eli Mintz <eli@dialean.com>
2020-02-23 16:31:19 -0500

    Revert: 0.0.7

--- test/joint_edit.py
+++
@@ -1,9 +0,0 @@
-import justpy as jp
-
-wp = jp.QuasarPage(delete_flag=False)
-editor = jp.QEditor(a=wp, kitchen_sink=True, height='94vh')
-
-def joint_edit():
-    return wp
-
-jp.justpy(joint_edit)
