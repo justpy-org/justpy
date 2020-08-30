702b1d86e8edca240f889519caf021f879db6311
Eli Mintz <eli@dialean.com>
2020-08-30 11:42:50 -0400

    0.1.4

--- justpy/templates/js/aggrid.js
+++ justpy/templates/js/aggrid.js
@@ -1,6 +1,7 @@
 // {* raw *}
 
 var cached_grid_def = {};
+
 Vue.component('grid', {
     template:
         `<div  v-bind:id="jp_props.id" :class="jp_props.classes"  :style="jp_props.style"  ></div>`,
@@ -8,16 +9,56 @@ Vue.component('grid', {
         grid_change() {
             var j = JSON.stringify(this.$props.jp_props.def);
             var grid_def = JSON.parse(j);  // Deep copy the grid definition
-            for (let i=0; i < this.$props.jp_props.html_columns.length; i++) {
-                grid_def.columnDefs[this.$props.jp_props.html_columns[i]].cellRenderer = function(params) {
-                            return params.value ? params.value : '';
-                        }
+            for (let i = 0; i < this.$props.jp_props.html_columns.length; i++) {
+                if (grid_def.columnDefs[this.$props.jp_props.html_columns[i]].cellRenderer === undefined)
+                grid_def.columnDefs[this.$props.jp_props.html_columns[i]].cellRenderer = function (params) {
+                    return params.value ? params.value : '';
+                };
             }
             cached_grid_def[this.$props.jp_props.id] = j;
             grid_def.onGridReady = grid_ready;
+            grid_def.popupParent = document.querySelector('body');
+            for (const field of this.$props.jp_props.evaluate) {
+                eval('grid_def[field] = ' + grid_def[field]);
+            }
+            // Code for CheckboxRenderer https://blog.ag-grid.com/binding-boolean-values-to-checkboxes-in-ag-grid/
+            function CheckboxRenderer() {
+            }
+
+            CheckboxRenderer.prototype.init = function (params) {
+                this.params = params;
+
+                this.eGui = document.createElement('input');
+                this.eGui.type = 'checkbox';
+                this.eGui.checked = params.value;
+
+                this.checkedHandler = this.checkedHandler.bind(this);
+                this.eGui.addEventListener('click', this.checkedHandler);
+            };
+
+            CheckboxRenderer.prototype.checkedHandler = function (e) {
+                let checked = e.target.checked;
+                let colId = this.params.column.colId;
+                this.params.node.setDataValue(colId, checked);
+            };
+
+            CheckboxRenderer.prototype.getGui = function (params) {
+                return this.eGui;
+            };
+
+            CheckboxRenderer.prototype.destroy = function (params) {
+                this.eGui.removeEventListener('click', this.checkedHandler);
+            };
+
+            grid_def.components = {
+                checkboxRenderer: CheckboxRenderer
+            };
+
+
             new agGrid.Grid(document.getElementById(this.$props.jp_props.id.toString()), grid_def);  // the api calls are added to grid_def
             cached_grid_def['g' + this.$props.jp_props.id] = grid_def;
             var auto_size = this.$props.jp_props.auto_size;
+
 
             function grid_ready(event) {
                 if (auto_size) {
@@ -85,7 +126,7 @@ Vue.component('grid', {
         if (JSON.stringify(this.$props.jp_props.def) != cached_grid_def[this.$props.jp_props.id]) {
             grid_to_destroy = cached_grid_def['g' + this.$props.jp_props.id];
             grid_to_destroy.api.destroy();
-            this.grid_change(); // Explore change to check difference and update with api instead of destroying and creating new grid
+            this.grid_change(); // Explore option to check difference and update with api instead of destroying and creating new grid
         }
     },
     props: {
