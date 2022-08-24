11859422bcb75545ab4fd154e4fc639e641dd5a9
Wolfgang Fahl <wf@bitplan.com>
2022-08-24 19:07:43 +0200

    adds justpy core test

---
+++ tests/test_justpy_core.py
@@ -0,0 +1,49 @@
+'''
+Created on 2022-08-24
+
+@author: wf
+'''
+# https://github.com/encode/starlette/blob/master/docs/testclient.md
+# https://stackoverflow.com/questions/57412825/how-to-start-a-uvicorn-fastapi-in-background-when-testing-with-pytest
+
+from multiprocessing import Process
+import justpy as jp
+import asynctest
+import aiohttp
+import asyncio
+
+class TestJustpyCore(asynctest.TestCase):
+    '''
+    Tests for Justpy Core features
+    '''
+    async def wp_to_test(self):
+        wp = jp.WebPage()
+        _d=jp.Div(a=wp)
+        return wp
+    
+    async def setUp(self):
+        """ Bring server up. """
+        self.port=8123
+        self.proc = Process(target=jp.justpy,
+                            args=(self.wp_to_test,),
+                            kwargs={
+                                "host": "127.0.0.1",
+                                "port": self.port,
+                            },
+                            daemon=True)
+        self.proc.start()
+        await asyncio.sleep(0.1)  # time for the server to start
+        
+    async def tearDown(self):
+        """ Shutdown the app. """
+        self.proc.terminate()
+        
+    
+    async def testWp(self):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(f"http://127.0.0.1:{self.port}/") as resp:
+                status = resp.status
+        self.assertEqual(200,status)        
+    
+    
+
