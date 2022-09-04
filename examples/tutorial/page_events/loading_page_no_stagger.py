11aecc6cbd0807375cd923f9f0bb624ddd8f318f
Wolfgang Fahl <wf@bitplan.com>
2022-09-04 13:22:20 +0200

    adds two more tutorial examples

---
+++ examples/tutorial/page_events/loading_page_no_stagger.py
@@ -0,0 +1,32 @@
+# Justpy Tutorial demo loading_page_no_stagger from docs/tutorial/page_events.md
+import justpy as jp
+import asyncio
+
+
+async def page_ready_div(self, msg):
+    for i in range(1,3001):
+        jp.Div(text=f'Div {i}', a=self.d, classes='border m-2 p-2 text-xs')
+        if i % 100 == 0:
+            await self.update()
+            await asyncio.sleep(0.25)
+
+
+@jp.SetRoute('/stagger')
+def loading_page_stagger_test():
+    wp = jp.WebPage()
+    wp.on('page_ready', page_ready_div)
+    wp.d = jp.Div(classes='flex flex-wrap', a=wp)
+    return wp
+
+
+def loading_page_no_stagger():
+    wp = jp.WebPage()
+    wp.d = jp.Div(classes='flex flex-wrap', a=wp)
+    for i in range(1, 3001):
+        jp.Div(text=f'Div {i}', a=wp.d, classes='border m-2 p-2 text-xs')
+    return wp
+
+
+# initialize the demo
+from  examples.basedemo import Demo
+Demo ("loading_page_no_stagger",loading_page_no_stagger)
---
+++ examples/tutorial/page_events/result_test.py
@@ -0,0 +1,28 @@
+# Justpy Tutorial demo result_test from docs/tutorial/page_events.md
+import justpy as jp
+
+js_string = """
+a = 3;
+b = 5;
+c = a * b;
+d = {r: c, appName: navigator.appName, appVersion: navigator.appVersion}
+
+"""
+
+async def page_ready(self, msg):
+    jp.run_task(self.run_javascript(js_string))
+
+async def result_ready(self, msg):
+    msg.page.result_div.text = f'The result is: {msg.result}'
+
+def result_test():
+    wp = jp.WebPage()
+    wp.on('page_ready', page_ready)
+    wp.on('result_ready', result_ready)
+    wp.result_div = jp.Div(text='Result will go here', classes='m-4 p-2 text-xl', a=wp)
+    jp.Pre(text=js_string, a=wp, classes='m-2 p-2 border')
+    return wp
+
+# initialize the demo
+from  examples.basedemo import Demo
+Demo ("result_test",result_test)
