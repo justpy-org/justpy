d14ba9fd01967d1bbd72c5c2c85df5313cb9d63f
Wolfgang Fahl <wf@bitplan.com>
2022-09-07 08:13:47 +0200

    adds routes examples from tutorial

---
+++ examples/tutorial/routes/route2.py
@@ -0,0 +1,19 @@
+# Justpy Tutorial demo  from docs/tutorial/routes.md
+import justpy as jp
+
+@jp.SetRoute('/hello')
+def hello_function3():
+    wp = jp.WebPage()
+    wp.add(jp.P(text='Hello there!', classes='text-5xl m-2'))
+    return wp
+
+@jp.SetRoute('/bye')
+def bye_function2():
+    wp = jp.WebPage()
+    wp.add(jp.P(text='Goodbye!', classes='text-5xl m-2'))
+    return wp
+
+
+# initialize the demo
+from  examples.basedemo import Demo
+Demo ("route2",None)
