e6fa18e9a913458ca1aa5c93567764841c03509c
Wolfgang Fahl <wf@bitplan.com>
2022-10-19 12:43:08 +0200

    fixes #567 and prepares release

--- jpcore/demostarter.py
+++ jpcore/demostarter.py
@@ -3,7 +3,6 @@ Created on 2022-09-22
 
 @author: wf
 """
-import asyncio
 import json
 import os
 import sys
@@ -20,19 +19,22 @@ class Demostarter:
     start justpy demos on different ports
     """
 
-    def __init__(self, mode: str = None, debug: bool = False):
+    def __init__(self, base_path:str=None,mode: str = None, debug: bool = False):
         """
         constructor
 
         Args:
+            base_path(str): the base_path
             debug(bool): if True switch on debug mode
         """
         Demo.testmode = True
         self.debug = debug
         self.mode = mode
         self.script_dir = os.path.dirname(__file__)
-        self.justpy_dir = f"{os.path.dirname(self.script_dir)}/examples"
-        self.example_json_file=f"{os.path.dirname(self.script_dir)}/tutorial/examples.json"
+        if base_path is None:
+            base_path=self.script_dir
+        self.justpy_dir = f"{base_path}/examples"
+        self.example_json_file=f"{os.path.dirname(base_path)}/tutorial/examples.json"
         if self.debug:
             print(f"collecting examples from {self.justpy_dir}")
         pymodule_files = find_files(self.justpy_dir, ".py")
@@ -163,6 +165,13 @@ def main(argv=None):  # IGNORE:C0111
             "--mode",
             default=None,
             help="the server start mode process/direct or None for os specific (default: %(default)s)",
+        )
+        script_dir = os.path.dirname(__file__)
+        parser.add_argument(
+            "-p",
+            "--path",
+            default=os.path.dirname(script_dir),
+            help="path to the examples (default: %(default)s)",
         )
         parser.add_argument(
             "--limit",
