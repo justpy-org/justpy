3c135bf6cc60431bd3c81fdcf7e887cc4d7608fa
Wolfgang Fahl <wf@bitplan.com>
2022-11-15 09:40:44 +0100

    fixes #620 by adding test

---
+++ tests/test_issue620.py
@@ -0,0 +1,31 @@
+"""
+Created on 2022-09-17
+
+@author: th
+"""
+from tests.basetest import Basetest
+from justpy.gridcomponents import AgGrid
+
+class TestIssue620(Basetest):
+    """
+    testing issue 620
+    https://github.com/justpy-org/justpy/issues/620
+    """
+
+    def test_issue_620(self):
+        """
+        test a lod loading
+        """
+        lod=[
+            {
+                "family_name":"Doe",
+                "given_name":"John",
+            }
+        ]
+        ag_grid=AgGrid()
+        ag_grid.load_lod(lod)
+        if self.debug:
+            print(ag_grid.options.columnDefs)
+        expected=[{'field': 'family_name'}, {'field': 'given_name'}]
+        self.assertEqual(expected, ag_grid.options.columnDefs)
+        
