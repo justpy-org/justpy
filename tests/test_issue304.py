e0a5e3f301805597c9d43f8f62e16f6eb6e2c797
Wolfgang Fahl <wf@bitplan.com>
2023-03-21 06:14:17 +0100

    increases timeout for test 304 from 5 to 10 secs

--- tests/test_issue304.py
+++ tests/test_issue304.py
@@ -33,8 +33,8 @@ class TestIssue304(BaseSeleniumTest):
         await self.server.start(self.issue_page)
         url = self.server.get_url("/")
         self.browser.get(url)
-        timeout = 5
-        driver = self.get_waiting_browser(self.browser)
+        timeout = 10.0
+        driver = self.get_waiting_browser(self.browser,timeout=timeout)
         # wait until a row is clickable
         self.assertTrue(
                 driver.until(
